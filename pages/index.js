df4286237bfb194692d174853f1bec4fb3d8e004
Jhonatan Ariza <arizaj@LIM915010.local>
2022-12-23 03:32:23 -0500

    hacer referencia los enlaces

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import useQuiosco from '../hooks/useQuiosco'
 import Layout from '../layout/Layout'
 
 
 export default function Home() {
+  const {categoriaActual} = useQuiosco()
   return (
-    <Layout>
-      <h1>Inicio</h1>
+    <Layout pagina={`Menú ${categoriaActual?.nombre}`}>
+      <h1 className='text-4xl fontblack'>Inicio</h1>
+      <p>Elige y personaliza tu pedido a c</p>
     </Layout>
   )
 }
